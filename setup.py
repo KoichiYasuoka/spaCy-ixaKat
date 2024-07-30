5dce03957d3cef259d52c42d2245f7a4d29e544d
Koichi Yasuoka <yasuoka@kanji.zinbun.kyoto-u.ac.jp>
2024-07-30 10:12:25 +0900

    version 0.6.8

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ packages=p.decode("utf-8").rstrip().split("\n")
 
 setuptools.setup(
   name="spacy_ixakat",
-  version="0.6.7",
+  version="0.6.8",
   description="ixaKat wrapper for spaCy",
   long_description=long_description,
   long_description_content_type="text/markdown",
@@ -28,7 +28,7 @@ setuptools.setup(
   keywords="ixaKat spaCy",
   packages=setuptools.find_packages(),
   package_data={"spacy_ixakat":packages},
-  install_requires=["spacy>=2.2.2","deplacy>=2.0.5"],
+  install_requires=["spacy>=2.2.2","deplacy>=2.1.0"],
   python_requires=">=3.6",
   classifiers=[
     "License :: OSI Approved :: MIT License",
